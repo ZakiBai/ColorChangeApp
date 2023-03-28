1d21816471676c7dee5e3938520bf023daee3051
Zaki <menza.zm@gmail.com>
2023-03-28 13:24:43 +0600

    logic of sliders

--- ColorChangeApp/ViewController.swift
+++ ColorChangeApp/ViewController.swift
@@ -8,8 +8,52 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    @IBOutlet var viewColor: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewColor.layer.cornerRadius = 15
+        
+        setColor()
+        
+        redLabel.text = string(from: redSlider)
+        greenLabel.text = string(from: greenSlider)
+        blueLabel.text = string(from: blueSlider)
+    }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
+        
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenLabel.text = string(from: greenSlider)
+        default:
+            blueLabel.text = string(from: blueSlider)
+        }
+    }
+    
+    private func setColor() {
+        viewColor.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func string(from: UISlider) -> String {
+        String(format: "%.2f", from.value)
     }
 }
